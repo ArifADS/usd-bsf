dcb9d81e309cec023d86d7e84e3e2cdae5466ccf
Arif <arifads@gmail.com>
2016-03-11 19:23:51 -0500

    First Commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var request = require("request")
+var port = process.env.PORT || 5000;
+
 
 app.use(express.static('public'));
 
@@ -39,6 +41,6 @@ app.get('/dolar', function (req, res) {
 
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
