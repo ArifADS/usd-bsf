b9aa7e74ccc6e18e712413636d4f04a23ab657c8
Arif De Sousa <arifads@gmail.com>
2018-03-09 23:08:21 +0100

    updated bin to folders and 2018 too (not tested)

--- bin/savePrice.js
+++ bin/savePrice.js
@@ -1,40 +1,25 @@
 #!/usr/bin/env node
-var request   = require("request")
-var mongoose  = require('mongoose');
+var axios = require("axios")
+var mongoose = require('mongoose');
+const { PriceHistory } = require("./server/models/")
 
 var url = "https://ddzcb7dwlckfq.cloudfront.net/custom/rate.js"
 var mongoURI = process.env.MONGOLAB_URI || "mongodb://heroku_53f0ml6l:qj1jq2e7593ark11281m0thq52@ds011369.mlab.com:11369/heroku_53f0ml6l";
 
 mongoose.connect(mongoURI);
 
-var phSchema = new mongoose.Schema({
-  dt       : Number,
-  simadi   : Number,
-  date     : Number
-}),
+axios(url).then(({ data }) => {
+  let obj = JSON.parse(data.replace("var dolartoday = \n", ""))
 
-PriceHistory = mongoose.model('PriceHistory', phSchema);
+  var dt = obj.USD.dolartoday
+  var sm = obj.USD.sicad2
 
-request(url, function (error, response, body) {
+  var price1 = new PriceHistory({ dt: dt, date: new Date().getTime() / 1000, simadi: sm })
 
-  if (!error && response.statusCode === 200) {
-
-    var obj = JSON.parse(body.replace("var dolartoday = \n",""))
-    var dt = obj.USD.dolartoday
-    var sm = obj.USD.sicad2
-
-    var price1 = new PriceHistory({dt:dt,date: new Date().getTime() / 1000,simadi: sm})
-
-    console.log(price1)
-
-    price1.save(function (err, userObj) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('saved successfully:');
-      }
-      mongoose.connection.close()
-    });
-
-  }
+  return price1.save()
 })
+.then(obj=>{
+  console.log('saved successfully:');
+  mongoose.connection.close()
+})
+.catch(console.error)
